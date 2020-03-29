78fadf76073bfb82a1de28391dd405a4f5c1a2dc
RoyaFiroozi <royafiroozi@berkeley.edu>
2020-03-28 20:22:23 -0700

    Update kinematic_bicycle_model.m

--- kinematic_bicycle_model.m
+++ kinematic_bicycle_model.m
@@ -1,9 +1,18 @@
 function [x,y,psi,v] = kinematic_bicycle_model(x,y,psi,v,delta_t,l_f,l_r,a,delta_f)
+% x is position in longitudinal direction
+% y is position in lateral direction
+% psi is heading angle
+% v is velocity (norm of velocity in x and y directions)
+% delta_t is sampling time
+% l_f is the length of the car from center of gravity to the front end
+% l_r is the length of the car from center of gravity to the rear end
+% a is acceleration which is control input
+% delta_f is steering angle which is control input 
 
 beta = atan(((l_r./(l_f+l_r))*tan(delta_f)));
-x = x + delta_t*v*cos(psi+beta); % position in x direction
-y = y + delta_t*v*sin(psi+beta); % position in y direction 
-% psi = psi + delta_t*(v./l_r)*sin(beta); % heading angle
-psi = psi + delta_t*(v*cos(beta)./(l_r+l_f)*tan(delta_f)); % heading angle
-v = v + delta_t*a; % vcloselocity (norm of velocity in x and y direction)
+x = x + delta_t*v*cos(psi+beta); 
+y = y + delta_t*v*sin(psi+beta); 
+% psi = psi + delta_t*(v./l_r)*sin(beta); 
+psi = psi + delta_t*(v*cos(beta)./(l_r+l_f)*tan(delta_f)); 
+v = v + delta_t*a; 
 end
